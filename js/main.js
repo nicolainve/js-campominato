f4f9b5ee0abaa38dff28979dacbaad1579903121
Nicola <nginvernizzi1@gmail.com>
2020-10-23 14:50:51 +0200

    array random

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('test');
+var arrayRandom = [];
+
+var size = 5;
+
+while (arrayRandom.length < size) {
+    var randomNumbers = getRandonNumber(1, 100);
+
+    if (! arrayRandom.includes(randomNumbers)) {
+        arrayRandom.push(randomNumbers);
+    }
+}
+
+console.log(arrayRandom);
+
+function getRandonNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
