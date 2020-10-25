ef0a87840a17bf03cfa6f648191287bb2161d075
Nicola <nginvernizzi1@gmail.com>
2020-10-25 19:15:03 +0100

    done

--- js/main.js
+++ js/main.js
@@ -1,33 +1,69 @@
-var arrayRandom = [];
+var bombNumber = 16;
 
-var size = 5;
+var nMax;
 
-while (arrayRandom.length < size) {
-    var randomNumbers = getRandomNumber(1, 100);
+var nMin = 1;
 
-    if (! arrayRandom.includes(randomNumbers)) {
-        arrayRandom.push(randomNumbers);
+var counter = 0;
+
+while ((isNaN(difficulty) || difficulty < 0 || difficulty > 2)) {
+    var difficulty = parseInt( prompt('Difficoltà') );
+}
+
+switch (difficulty) {
+    case 0:
+        nMax = 100;
+        break;
+    case 1:
+        nMax = 80;
+        break;
+    case 2:
+        nMax = 50;
+        break;
+}
+
+
+var bombList = pushArray(bombNumber, nMax);
+
+console.log(bombList);
+
+var userList = [];
+
+for (var i = 0; i < (nMax - bombNumber); i++) {
+    while (isNaN(userNumber) || userNumber < nMin || userNumber > nMax || userList.includes(userNumber)) {
+        var userNumber = parseInt( prompt('Inserisci un numero, VALIDO, compreso tra ' + nMin + ' e ' + nMax) );
     }
+
+    if (! bombList.includes(userNumber) ) {
+        alert('Continua');
+        userList.push(userNumber);
+        counter++
+    } else {
+        alert('Hai perso');
+        break;
+    }
+
+    console.log(userList);
+
+    console.log(counter);
 }
 
-console.log(arrayRandom);
-
-
-while (! isInArray(userNumber, arrayRandom)) {
-    var userNumber = prompt('inserisci');
-    console.log(isInArray(userNumber, arrayRandom));
+if ((nMax - bombNumber) == counter) {
+    alert('Hai vinto')
 }
 
 
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-function isInArray(val, array) {
-    for (var i = 0; i < array.length; i++) { 
-        if (array[i] == val ) {
-            return true;
-        } 
-    } 
-    return false;
+function pushArray(num, max) {
+    var bombList = [];
+    while (bombList.length < num) {
+
+        var numero = Math.round ( Math.random() * max ) + 1;
+
+        if (! bombList.includes(numero)) {
+            bombList.push(numero);
+        }
+    }
+
+    return bombList
 }
